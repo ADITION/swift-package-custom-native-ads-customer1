8fa5ecc1d1986c5d94b5d41470c9b36e5515ebb7
VDPurohit <114067496+aditionmobileasdk@users.noreply.github.com>
2023-01-31 14:27:10 +0100

    updated version 1.0.2beta

--- Package.swift
+++ Package.swift
@@ -22,7 +22,7 @@ let package = Package(
             name: "NativeAdsPackage",
             dependencies: []),
         .binaryTarget(name: "NativeAds",
-                             url:"https://developer.adition.com/spm/service/local/repositories/releases/content/adition/ios/nativeads/1.0.1-beta/nativeads-1.0.1-beta.xcframework.zip",
-                             checksum: "d563ff8e6dcf59cc19542e8f1e1b1764276a42f0b990d184c30e82f1628f58bc")
+                             url:"https://developer.adition.com/spm/service/local/repositories/releases/content/adition/ios/nativeads/1.0.2-beta/nativeads-1.0.2-beta.zip",
+                             checksum: "5090d4dceed16ee87ee9ce959a899b1108bb2924b4a787e1d49678e712b466fc")
     ]
 )
